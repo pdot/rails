b49de881af3e1d8471af677e1c5a522cbd7634f6
Your Name <admin@pdot.ca>
2010-01-18 18:23:44 -0800

    Possbible solution to smtp issue.

--- config/initializers/smtp_gmail.rb
+++ config/initializers/smtp_gmail.rb
@@ -1,6 +1,10 @@
 # This file is automatically copied into RAILS_ROOT/initializers
 
-require "smtp_tls"
+ruby_version = /([0-9]+)\.([0-9]+)\.([0-9]+)/.match(`ruby -v`)
+if ruby_version[1].to_i <= 1 and ruby_version[2].to_i <= 8 and
+ruby_version[3].to_i <= 6
+  require "smtp_tls"
+end
 
 config_file = "#{RAILS_ROOT}/config/smtp_gmail.yml"
 raise "Sorry, you must have #{config_file}" unless File.exists?(config_file)
